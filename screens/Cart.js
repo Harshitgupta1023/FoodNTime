ea5f50d3e52a2accd72b205f6058c1f9111eb406
gourav2001k <mailatgourav2001@gmail.com>
2021-05-05 00:48:27 +0530

    Added Google Auth

--- screens/Cart.js
+++ screens/Cart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-const Cart = async () => {
+const Cart = (props) => {
   return (
     <View style={styles.screen}>
       <Text>CART</Text>
--- screens/Login.js
+++ screens/Login.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Input, Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-import firebase from "../config/Firebase";
+import firebase from "firebase";
+import * as Google from "expo-google-app-auth";
+import Firebase from "../config/Firebase";
 // import * as ImagePicker from "expo-image-picker";
 
 const LoginScreen = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const onLogIn = async () => {
-    await firebase.auth().signInWithEmailAndPassword(email, password);
-    var user = firebase.auth().currentUser;
+    await Firebase.auth().signInWithEmailAndPassword(email, password);
+    var user = Firebase.auth().currentUser;
     if (user) {
       props.navigation.replace("Food N Time");
     }
@@ -30,9 +33,70 @@ const LoginScreen = (props) => {
     // console.log(await storageRef.child(loc).getDownloadURL());
     // storageRef.child(loc).delete();
   };
+  const onGoogleLogin = async () => {
+    const isUserEqual = (googleUser, firebaseUser) => {
+      if (firebaseUser) {
+        var providerData = firebaseUser.providerData;
+        for (var i = 0; i < providerData.length; i++) {
+          if (
+            providerData[i].providerId ===
+              firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+            providerData[i].uid === googleUser.user.id
+          ) {
+            // We don't need to reauth the Firebase connection.
+            return true;
+          }
+        }
+      }
+      return false;
+    };
+    const onSignIn = (googleUser) => {
+      // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+      var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!isUserEqual(googleUser, firebaseUser)) {
+          // Build Firebase credential with the Google ID token.
+          var credential = firebase.auth.GoogleAuthProvider.credential(
+            googleUser.idToken
+          );
+
+          // Sign in with credential from the Google user.
+          firebase
+            .auth()
+            .signInWithCredential(credential)
+            .then((res) => {
+              props.navigation.replace("Food N Time");
+            })
+            .catch((error) => {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+              // ...
+            });
+        } else {
+          console.log("User already signed-in Firebase.");
+          props.navigation.replace("Food N Time");
+        }
+      });
+    };
+    try {
+      const googleUser = await Google.logInAsync({
+        androidClientId: process.env.GOOGLE_CLIENT_ID,
+      });
+      if (googleUser.type === "success") {
+        onSignIn(googleUser);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <View style={styles.screen}>
-      <View></View>
       <View style={styles.inputcontainer}>
         <Input
           placeholder="email@address.com"
@@ -48,12 +112,27 @@ const LoginScreen = (props) => {
           leftIcon={{ type: "font-awesome", name: "lock" }}
         />
       </View>
-      <Button
-        raised={true}
-        title="Log In"
-        onPress={onLogIn}
-        buttonStyle={{ width: 100 }}
-      />
+      <View style={styles.button}>
+        <Button
+          raised={true}
+          title="Log In"
+          onPress={onLogIn}
+          buttonStyle={{ width: 100 }}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button
+          icon={<Icon name="google" size={25} color="white" />}
+          raised={true}
+          title="     SignIn with Google"
+          onPress={onGoogleLogin}
+          buttonStyle={{
+            width: 200,
+            backgroundColor: "red",
+            borderRadius: 30,
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -66,6 +145,9 @@ const styles = StyleSheet.create({
   },
   inputcontainer: {
     width: "80%",
+  },
+  button: {
+    marginVertical: 10,
   },
 });
 export default LoginScreen;
