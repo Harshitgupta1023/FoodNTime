c4416f7e0567cd7682db12aa9f546bd8b2a807a0
gourav2001k <mailatgourav2001@gmail.com>
2021-05-05 18:15:51 +0530

    Navigation Header Configured

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -2,11 +2,15 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { FontAwesome } from "@expo/vector-icons";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import StartUpScreen from "../screens/GetStarted";
 import LoginScreen from "../screens/Login";
 import SignUpScreen from "../screens/SignUp";
 import UserNavigator from "./UserNavigator";
+import Cart from "../screens/Cart";
+import HeaderButtonss from "../components/HeaderButtonss";
 
 import { Text } from "react-native";
 import VendorNavigator from "./VendorNavigator";
@@ -51,7 +55,12 @@ const AuthNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen name="StartUp" component={StartUpScreen} />
       <Stack.Screen name="Authentication" component={MainNavigator} />
-      <Stack.Screen name="Food N Time" component={UserNavigator} />
+      <Stack.Screen
+        name="Food N Time"
+        component={UserNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Cart" component={Cart} />
       <Stack.Screen name="Vendor Dashboard" component={VendorNavigator} />
     </Stack.Navigator>
   );
